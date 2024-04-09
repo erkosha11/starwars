38fc2d45f9c1b8efad1b3888e0370f1dcc2b8206
Erkosha <erkebulan003@yandex.ru>
2024-04-09 19:08:23 +0500

    add validation in login and signup

--- src/store/signup-store.tsx
+++ src/store/signup-store.tsx
@@ -1,35 +1,43 @@
 import { makeAutoObservable } from "mobx";
 
-class InputStore {
-  inpData = {
+interface InpData {
+  login: string;
+  password: string;
+  confirmpassword: string;
+}
+
+class SignUpStore {
+  inpData: InpData = {
     login: "",
     password: "",
+    confirmpassword: "",
   };
 
   inpDataErr = {
     loginErr: "",
     passwordErr: "",
+    confirmpasswordErr: "",
   };
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  updateInpData = (key: string, value: string) => {
+  updateInpData = (key: keyof InpData, value: string) => {
     this.inpDataErr = {
       ...this.inpDataErr,
       [key + "Err"]: "",
     };
-    this.inpData = {
-      ...this.inpData,
-      [key]: value,
-    };
+    this.inpData[key] = value;
+    if (key === 'password' && this.inpData.confirmpassword !== "") {
+      this.validateData();
+    }
   };
 
   clickHandler = (navigateCallback: Function) => {
     this.validateData();
     if (Object.values(this.inpDataErr).some((i) => i !== "")) return;
-    navigateCallback("/main");
+    navigateCallback("/login");
   };
 
   validateData = () => {
@@ -44,7 +52,12 @@ class InputStore {
       !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\S]+$/.test(this.inpData.password)
     )
       this.inpDataErr.passwordErr = "Ваш пароль слишком простой";
+
+    if (this.inpData.confirmpassword === "")
+      this.inpDataErr.confirmpasswordErr = "Подтвердите пароль";
+    else if (this.inpData.confirmpassword !== this.inpData.password)
+      this.inpDataErr.confirmpasswordErr = "Пароли не совпадают";
   };
 }
 
-export const inputStore = new InputStore();
+export const signUpStore = new SignUpStore();
